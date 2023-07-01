23ab22f2a6b5180a1700a0276ef601aa4eae4fa8
Pavel Bu <puchik006@mail.ru>
2023-07-01 03:33:26 +0300

    Update PrefabInstantiator.cs

--- Assets/Scripts/PrefabInstantiator.cs
+++ Assets/Scripts/PrefabInstantiator.cs
@@ -22,7 +22,16 @@ public class PrefabInstantiator
         _content = content;
         _context = context;
 
+        MainSceneButtonHandler.OnButtonPressed += SetInstantiatorToWork;
+        ExitButtonHandler.OnButtonPressed += SetInstantiatorToWork;
+
         _prefabImageLoader = new PrefabImageLoader();
+    }
+
+    private void SetInstantiatorToWork()
+    {
+        _currentImageNumber = 1;
+        _currentLoadingImage = 1;
     }
 
     public void LoadAndInstantiatePrefab()
@@ -61,75 +70,3 @@ public class PrefabInstantiator
         });
     }
 }
-
-//public class PrefabInstantiator
-//{
-//    private MonoBehaviour _context;
-//    private PrefabImageLoader _prefabImageLoader;
-//    private static List<int> _imageNumbersList = new();
-//    public static Action OnButtonPressed;
-
-//    private RectTransform _content;
-//    private int _currentImageNumber = 1;
-//    private int _currentLoadingImage = 1;
-//    private GameObject _prefab;
-
-//    public PrefabInstantiator(GameObject prefab, RectTransform content, MonoBehaviour context)
-//    {
-//        _prefab = prefab;
-//        _content = content;
-//        _context = context;
-
-//        MainSceneButtonHandler.OnButtonPressed += () => _imageNumbersList.Clear();
-//        ExitButtonHandler.OnButtonPressed += () => _imageNumbersList.Clear();
-//        _prefabImageLoader = new PrefabImageLoader();
-//    }
-
-//    public void LoadAndInstantiatePrefab()
-//    {
-//        int picNumber = _currentImageNumber;
-//        _currentImageNumber++;
-
-//        _context.StartCoroutine(_prefabImageLoader.LoadImage(picNumber, OnImageLoaded));
-//    }
-
-//    private Dictionary<int?, Sprite> spriteDict = new();
-
-//    private void OnImageLoaded(Sprite sprite, int? loadedPictureNumber)
-//    {
-//        if (sprite == null) return;
-//        spriteDict.Add(loadedPictureNumber, sprite);
-//        GetSpriteFromDictionary();
-//    }
-
-
-//    private void GetSpriteFromDictionary()
-//    {
-//        if (spriteDict.Count == 0) return;
-
-//        for (int i = 1; i <= _currentLoadingImage; i++)
-//        {
-//            if (spriteDict.ContainsKey(i))
-//            {
-//                InstantiateAndSetPrefabBehaviour(spriteDict[i]);
-//                spriteDict.Remove(i);
-//                Debug.Log(i + " cli " + _currentLoadingImage);
-//            }
-//        }
-//        _currentLoadingImage++;
-//    }
-
-//    private void InstantiateAndSetPrefabBehaviour(Sprite sprite)
-//    {
-//        var prefab = Object.Instantiate(_prefab, _content);
-
-//        prefab.GetComponentInChildren<Image>().sprite = sprite;
-//        prefab.GetComponentInChildren<Button>().onClick.AddListener(() =>
-//        {
-//            OnButtonPressed?.Invoke();
-//            ImageHolder._tempSprite = sprite;
-//        });
-
-//        _imageNumbersList.Add(_currentImageNumber);
-//    }
-//}
